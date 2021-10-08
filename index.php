4f37722de60c474c07c2601bdb32bd45c59eba57
jiseong <jisung87kr@nate.com>
2021-10-09 00:17:51 +0900

    DataTable 라이브러리 적용 (refs #42)

--- index.php
+++ index.php
@@ -12,26 +12,34 @@ $myCrawler = $app->make(SearchAD::class);
 $DEBUG = false;
 $myCrawler->init($config['BASE_URL'], $config['API_KEY'], $config['SECRET_KEY'], $config['CUSTOMER_ID']);
 
-if(isset($_GET['keyword'])){
-    $keyword = $_GET['keyword'];
-    $params = [
-        'siteId' => 'bsn-a001-00-000000004370037',
-        'biztpId' => 1,
-        'hintKeywords' => $keyword,
-        'event' => 1,
-        'month' => date('n'),
-        'showDetail' => 1
-    ];
-    $result = $myCrawler->GET('/keywordstool', $params);
-}
+if(isset($_REQUEST['act'])){
+    header('Content-Type: application/json; charset=UTF-8');
+    $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+    switch($_REQUEST['act']){
+        case 'keywordList':
+            $params = [
+                'siteId' => 'bsn-a001-00-000000004370037',
+                'biztpId' => 1,
+                'hintKeywords' => $keyword,
+                'event' => 1,
+                'month' => date('n'),
+                'showDetail' => 1
+            ];
+            $result = $myCrawler->GET('/keywordstool', $params);
+            $json = json_encode($result, JSON_UNESCAPED_UNICODE);
+            $json = str_replace('< ', '', $json);
+//            $myCrawler->dd($result);
+//            $myCrawler->dd($json);
+            break;
+        case 'crawlNaverShopping':
+            $crawlData = $myCrawler->crawlNaverShopping($keyword);
+            $json =  html_entity_decode($crawlData[0]);
+            break;
+    }
 
-if(isset($_REQUEST['act']) && $_REQUEST['act'] == 'crawlNaverShopping'){
-    $keyword = $_GET['keyword'];
-    $crawlData = $myCrawler->crawlNaverShopping($keyword);
-    echo html_entity_decode($crawlData[0]);
+    echo $json;
     exit;
 }
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -60,21 +68,20 @@ if(isset($_REQUEST['act']) && $_REQUEST['act'] == 'crawlNaverShopping'){
 <body>
 <div class="container-fluid">
     <div class="border my-5">
-        <form action="" method="GET" class="my-3">
+        <form action="" class="my-3" id="search-form">
             <div class="row g-3 justify-content-center">
                 <div class="col-auto">
-                    <input type="text" name="keyword" value="<?= $keyword ?>" placeholder="키워드 검색" class="form-control">
+                    <input type="text" name="keyword" value="감자" placeholder="키워드 검색" class="form-control">
                 </div>
                 <div class="col-auto">
-                    <input type="submit" value="검색" class="btn btn-primary">
+                    <input type="submit" value="검색" class="btn btn-primary btn-search">
                 </div>
             </div>
         </form>
     </div>
     <div class="table-responsive">
-        <?php if(isset($keyword)): ?>
-        <h1>'<?= $keyword ?>' 키워드 검색 결과 (<?= count($result['keywordList']); ?>건)</h1>
-        <table class="table table-bordered table-striped table-hover" style="font-size: 12px;" id="table">
+        <h1>키워드 검색결과</h1>
+        <table class="table table-bordered table-striped table-hover" style="font-size: 12px;" id="dataTable">
             <thead>
             <tr>
                 <td>#</td>
@@ -87,72 +94,90 @@ if(isset($_REQUEST['act']) && $_REQUEST['act'] == 'crawlNaverShopping'){
                 <td>MO평균클릭률</td>
                 <td>월평균노출광고</td>
                 <td>경쟁도</td>
+                <td>상품수</td>
                 <td>추가정보</td>
             </tr>
             </thead>
             <tbody>
-            <?php foreach ($result['keywordList'] as $index => $item) : ?>
-                <tr>
-                    <td><?= $index ?></td>
-                    <td><?= $item['relKeyword'] ?></td>
-                    <td><?= $item['monthlyPcQcCnt'] ?></td>
-                    <td><?= $item['monthlyMobileQcCnt'] ?></td>
-                    <td><?= $item['monthlyAvePcClkCnt'] ?></td>
-                    <td><?= $item['monthlyAveMobileClkCnt'] ?></td>
-                    <td><?= $item['monthlyAvePcCtr'] ?></td>
-                    <td><?= $item['monthlyAveMobileCtr'] ?></td>
-                    <td><?= $item['plAvgDepth'] ?></td>
-                    <td><?= $item['compIdx'] ?></td>
-                    <td>
-                        <input type="button" value="추가정보" class="btn btn-secondary btn-sm btn-more" data-keyword="<?= $item['relKeyword'] ?>">
-                    </td>
-                </tr>
-            <?php endforeach; ?>
             </tbody>
         </table>
-        <?php else: ?>
-        <div class="text-center my-3">
-            <h3>검색어를 입력하세요.</h3>
-        </div>
-        <?php endif;?>
-    </div>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 <script>
     $(document).ready(function() {
-        var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-        function moreData(_this){
-            var keyword = _this.data('keyword');
+        var tb;
+        function childData(data, _this){
+            var total = data.props.pageProps.initialState.products.total;
+            var tr = _this.closest('tr');
+            var cell = tb.cell(tr, 10);
+            cell.data(total);
+            return '<div>'+total+'</div>';
+        }
+
+        function initDataTable(el, data){
+            var dtOpt = {
+                paging: false,
+                stateSave: true,
+                fixedHeader: {
+                    header: true,
+                },
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ],
+                data: data,
+                columns: [
+                    {data: 'idx'},
+                    {data: 'relKeyword'},
+                    {data: 'monthlyPcQcCnt'},
+                    {data: 'monthlyMobileQcCnt'},
+                    {data: 'monthlyAvePcClkCnt'},
+                    {data: 'monthlyAveMobileClkCnt'},
+                    {data: 'monthlyAvePcCtr'},
+                    {data: 'monthlyAveMobileCtr'},
+                    {data: 'plAvgDepth'},
+                    {data: 'compIdx'},
+                    {data: 'total'},
+                    {defaultContent: '<input type="button" value="추가정보" class="btn btn-secondary btn-sm btn-more" />'},
+                ]
+            };
+
+            return el.DataTable(dtOpt);
+        }
+
+        function getKeywordList(keyword){
+            axios.get('./', {
+                params: {
+                    act: 'keywordList',
+                    keyword: keyword,
+                }
+            }).then(function(response){
+                var data = response.data;
+                for (var i = 0; i < data.keywordList.length; i++) {
+                    data.keywordList[i].idx = i+1;
+                    data.keywordList[i].total = '';
+                }
+                tb = initDataTable($("#dataTable"), data.keywordList);
+            }).catch(function(error){
+                console.log(error);
+            })
+        }
+
+        function getCrawlNaverShopping(_this){
+            var keyword = _this.closest('tr').find('td').eq(1).text();
             startLoading(true);
             axios.get('./', {
                 params : {
+                    act: 'crawlNaverShopping',
                     keyword: keyword,
-                    act: 'crawlNaverShopping'
                 }
             })
             .then(function(response) {
                 var data = response.data;
-                console.log(_this);
+                // var products = data.props.pageProps.initialState.products;
+                // console.log(products);
                 _this.attr('data-response', JSON.stringify(data));
-                console.log(data);
-                myModal.show();
+                drawChild(_this, data);
                 startLoading(false);
             })
             .catch(function(error) {
@@ -168,55 +193,38 @@ if(isset($_REQUEST['act']) && $_REQUEST['act'] == 'crawlNaverShopping'){
             }
         }
 
-        function format () {
-            // `d` is the original data object for the row
-            return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-                '<tr>'+
-                '<td>Full name:</td>'+
-                '<td>'+1+'</td>'+
-                '</tr>'+
-                '<tr>'+
-                '<td>Extension number:</td>'+
-                '<td>'+2+'</td>'+
-                '</tr>'+
-                '<tr>'+
-                '<td>Extra info:</td>'+
-                '<td>And any further details here (images etc)...</td>'+
-                '</tr>'+
-                '</table>';
-        }
-
-        var opt = {
-            paging: false,
-            fixedHeader: {
-                header: true,
-            },
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ]
-        };
-
-        var table = $('#table').dataTable(opt);
-        console.log(table.row());
-        $(".btn-more").click(function(){
-            moreData($(this));
-
-            var tr = $(this).closest('tr');
-            var row = table.row( tr );
+        function drawChild(_this, data)
+        {
+            var tr = _this.closest('tr');
+            var row = tb.row( tr );
 
             if ( row.child.isShown() ) {
-                // This row is already open - close it
                 row.child.hide();
                 tr.removeClass('shown');
             }
             else {
-                // Open this row
-                row.child( format(row.data()) ).show();
+                if(data){
+                    row.child( childData(data, _this) ).show();
+                } else {
+                    row.child.show();
+                }
                 tr.addClass('shown');
             }
+        }
 
+        $(".btn-search").click(function(event){
+            event.preventDefault();
+            var keyword = $(this).closest("form").find("input[name=keyword]").val();
+            getKeywordList(keyword);
+        });
 
+        $("html").on("click", ".btn-more", function(){
+            var hasReponse = $(this).attr('data-response');
+            if(hasReponse){
+                drawChild($(this), false);
+            } else {
+                getCrawlNaverShopping($(this));
+            }
         });
 
         $("#exampleModal").on("show.bs.modal", function(event){
@@ -226,8 +234,6 @@ if(isset($_REQUEST['act']) && $_REQUEST['act'] == 'crawlNaverShopping'){
         $("#exampleModal").on("hidden.bs.modal", function(event){
             console.log(event);
         });
-
-
     });
 </script>
 </body>
